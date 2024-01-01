3e4630f1483fef957db971a200a5c9fae864657f
Farhad Rakib <rakib.farhad@surbanajurong.com>
2024-01-01 16:16:00 +0600

    Add project files.

---
+++ Refit/Interfaces/IUsersClient.cs
@@ -0,0 +1,23 @@
+﻿using Refit;
+using RefitExample.Models;
+
+namespace RefitExample.Interfaces
+{
+    interface IUsersClient
+    {
+        [Get("/users")]
+        Task<IEnumerable<Users>> GetAll();
+
+        [Get("/users/{id}")]
+        Task<Users> GetUser(int id);
+
+        [Post("/users")]
+        Task<Users> CreateUser([Body] Users user);
+
+        [Put("/users/{id}")]
+        Task<Users> UpdateUser(int id, [Body] Users user);
+
+        [Delete("/users/{id}")]
+        Task DeleteUser(int id);
+    }
+}
---
+++ Refit/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RefitExample.Interfaces;
+using RefitExample.Models;
+
+namespace Refit;
+public class Program
+{
+    static async Task Main(string[] args)
+    {
+        using IHost host = Host.CreateDefaultBuilder(args)
+        .ConfigureServices((_, services) =>
+        {
+            services
+                .AddRefitClient<IUsersClient>()
+                .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/"));
+        }).Build();
+
+        var usersClient = host.Services.GetRequiredService<IUsersClient>();
+
+        //Get All user
+        var users = await usersClient.GetAll();
+        foreach (var usr in users)
+        {
+            Console.WriteLine(usr);
+        }
+
+        // create api end point test
+        var user = new Users
+        {
+            Name = "John Doe",
+            Email = "john.doe@test.com"
+        };
+        
+        var userId = (await usersClient.CreateUser(user)).Id;
+        Console.WriteLine($"User with Id: {userId} created");
+
+        // Get a single User
+        Console.WriteLine("User with Id  10 is : " +await usersClient.GetUser(10) );
+    }
+}
